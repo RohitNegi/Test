fdfd4247c3f6708fd64567b5001edb51c6979854
Rohit Negi <rohitnegi017@gmail.com>
2019-12-23 23:29:13 +0530

    initial commit

---
+++ TestPodDependency/Source/Fab.swift
@@ -0,0 +1,16 @@
+//
+//  Fab.swift
+//  MIFabDemo
+//
+//  Created by Rohit Negi on 23/12/19.
+//  Copyright © 2019 Mario Iannotta. All rights reserved.
+//
+
+import Foundation
+//import MIFab
+import SimpleKeychain
+class Fab{
+    func checkFab(){
+        let s = A0SimpleKeychain.init(service: "test")
+    }
+}
---
+++ TestPodDependency/TestPodDependency.h
@@ -0,0 +1,19 @@
+//
+//  TestPodDependency.h
+//  TestPodDependency
+//
+//  Created by Rohit Negi on 23/12/19.
+//  Copyright © 2019 Rohit Negi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPodDependency.
+FOUNDATION_EXPORT double TestPodDependencyVersionNumber;
+
+//! Project version string for TestPodDependency.
+FOUNDATION_EXPORT const unsigned char TestPodDependencyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodDependency/PublicHeader.h>
+
+
